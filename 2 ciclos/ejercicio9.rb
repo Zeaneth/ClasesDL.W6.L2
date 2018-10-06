96f6c78d3350f260ac5593b988f5f3d5e1647141
Zamiz <zamiztobar@gmail.com>
2018-10-06 16:18:36 -0300

    +Ejercicio 9

--- 2 ciclos/ejercicio9.rb
+++ 2 ciclos/ejercicio9.rb
@@ -11,3 +11,18 @@
 </table>
 
 =end
+
+def tabla
+	indent = "    "
+	puts "<table>"
+	puts "#{indent}<tbody>"
+	puts "#{indent*2}<tr>"
+	puts "#{indent*3}<td> 1 </td>"
+	puts "#{indent*3}<td> 2 </td>"
+	puts "#{indent*3}<td> 3 </td>"
+	puts "#{indent*2}</tr>"
+	puts "#{indent}</tbody>"
+	puts "</table>"
+end
+
+tabla
