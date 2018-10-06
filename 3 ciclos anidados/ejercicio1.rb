b0d322bbce6bb0825bdd757d5a61408171fa9dac
Zamiz <zamiztobar@gmail.com>
2018-10-06 18:02:02 -0300

    +Ejercicio 1

--- 3 ciclos anidados/ejercicio1.rb
+++ 3 ciclos anidados/ejercicio1.rb
@@ -5,3 +5,30 @@ Se pide imprimir la secuencia numérica, de la siguiente forma:
 3   6   9   12
 4   8   12   16
 =end
+
+def tabla
+    a = '  '
+    i = 1
+    4.times do |i|
+        i += 1
+        print "#{i}#{a}"
+    end
+    puts a
+    4.times do |i|
+        i *= 2
+        print "#{i+2}#{a}"
+    end
+    puts a
+    4.times do |i|
+        i *= 3
+        print "#{i+3}#{a}"
+    end
+    puts a
+    4.times do |i|
+        i *= 4
+        print "#{i+4}#{a}"
+    end
+end 
+
+puts tabla
+#no sé por qué me aparece el 4 :c
